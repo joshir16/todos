4f416edaa8bda9df2a88cedc37876843496e2e05
Rishabh <joshir838@gmail.com>
2024-08-05 18:25:48 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(task) {
+    setItems((items) => [...items, task]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Form onAddItems={handleAddItems} />
+      <TodoList items={items} />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return <h1>Todos</h1>;
+}
+
+function Form({ onAddItems }) {
+  const [task, setTask] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!task) return;
+
+    const newTask = { task, id: Date.now() };
+
+    onAddItems(newTask);
+
+    setTask("");
+  }
+
+  return (
+    <form className="form" onClick={handleSubmit}>
+      <h3>What do you want todo?</h3>
+      <input
+        type="text"
+        placeholder="add items..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      ></input>
+      <button className="btn-add">Add</button>
+    </form>
+  );
+}
+
+function TodoList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <span>{item.task}</span>
+      <span>❌</span>
+    </li>
+  );
+}
